c54f95e1e13cc708f98bef11560cad139e292de0
MatthiasMifsud <matthiasmifsud3@gmail.com>
2025-03-24 12:52:28 +0100

    change

--- API/main.py
+++ API/main.py
@@ -1,7 +1,7 @@
 from combiner import Combiner
 
 if __name__ == "__main__":
-    api_key = 'ba980974734d4bec81edfdf6412877c5'
+    api_key = 'YOUR_API_KEY'
     source_list = [
         "bbc", "cnn", "nytimes", "theguardian", "reuters", "foxnews",
         "bbc-news", "cbs-news", "abc-news", "huffpost"
